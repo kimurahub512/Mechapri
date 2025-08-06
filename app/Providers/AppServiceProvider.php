3dda17fb650a7bc82837efa25c07005f404ae319
Ramon Lopez <ramon@weeatbig.com>
2025-08-06 17:28:29 +0900

    -added facades to serviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Vite::prefetch(concurrency: 3);
         if (app()->environment('production')) {
-            \URL::forceScheme('https');
+            URL::forceScheme('https');
         }
         
     }
