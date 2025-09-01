bd7f18a5ca93ce30a9149e185b4cf17679164e6f
Ramon Lopez <ramon@weeatbig.com>
2025-09-01 17:13:41 +0900

    -removed dashboard link on the header

--- resources/js/Components/header/header.jsx
+++ resources/js/Components/header/header.jsx
@@ -21,14 +21,14 @@ const Header = ({ authButton }) => {
           <a href='/' className='flex items-center'>
             <img src={logo} alt='mechapuri Logo' style={{ ...responsiveMetricD(224, 58) }} />
           </a>
-          {auth?.user?.user_type === 'admin' && (
+          {/* {auth?.user?.user_type === 'admin' && (
           <a href='/dashboard' className="p-2 text-gray-400 hover:text-gray-600">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-5 5v-5z" />
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
             </svg>
           </a>
-          )}
+          )} */}
           <div className='flex flex-col items-end' style={{ width: vwd(666) }}>
             <div className='flex flex-row items-center justify-end' style={{ width: vwd(366), gap: vwd(20) }}>
               {/* ログイン: only show if not logged in */}
